fafbebc881984a511ceab6d9623b787838370e3a
Alec Thompson <ajthompson@wpi.edu>
2014-11-23 16:26:50 -0500

    Fixed the obstacle expansion

--- src/lab4/scripts/obstacle_expansion.py
+++ src/lab4/scripts/obstacle_expansion.py
@@ -5,16 +5,19 @@ import rospy
 import roslib
 import copy
 from nav_msgs.msg import OccupancyGrid
+from map_msgs.msg import OccupancyGridUpdate
 
 # Map topic subscriber callback
 def read_map(msg):
+	global expanded_pub
+	global current_map
 	current_map = msg
 	expanded_map = expand_obstacles(current_map)
+	expanded_pub.publish(expanded_map)
+	print "Expanded map published"
 
 def expand_obstacles(map_in):
-	global expanded
 	expanded = copy.deepcopy(map_in)
-	global expanded_pub
 
 	width = expanded.info.width
 	height = expanded.info.height
@@ -55,15 +58,49 @@ def expand_obstacles(map_in):
 								modified[l*width + k] = 1
 
 	expanded.data = new_data
-	expanded_pub.publish(expanded)
-	print "Expanded map published"
+	return expanded
+
+def update_map(msg):
+	global expanded_pub
+	global current_map
+	update = msg
+	x = 0
+	y = 0
+
+	try:
+		test = current_map
+	except NameError:
+		current_map = None
+
+	while current_map== None and not rospy.is_shutdown():
+		pass
+
+	new_data = [0] * current_map.info.width * current_map.info.height
+
+	for i in range(current_map.info.width*current_map.info.height):
+		new_data[i] = current_map.data[i]
+
+	for i in range(update.x, update.x + update.width):
+		for j in range(update.y, update.y + update.height):
+			new_data[j*current_map.info.width + i] = update.data[y*update.width + x]
+			y += 1
+		y = 0
+		x += 1
+
+	current_map.data = new_data
+	expanded_map = expand_obstacles(current_map)
+	expanded_pub.publish(expanded_map)
+	print "Expanded update published"
 
 if __name__ == '__main__':
 	rospy.init_node('obstacle_expansion')
 
 	global expanded_pub
+	global unexpanded
+	global current_map
 
-	map_sub = rospy.Subscriber('/map', OccupancyGrid, read_map, queue_size=1) #Callback function to handle mapping
+	map_sub = rospy.Subscriber('/move_base/global_costmap/costmap', OccupancyGrid, read_map, queue_size=1) #Callback function to handle mapping
+	update_sub = rospy.Subscriber('/move_base/global_costmap/costmap_updates', OccupancyGridUpdate, update_map, queue_size=1)
 	expanded_pub = rospy.Publisher('/expandedmap', OccupancyGrid)
 
 	rospy.spin()
