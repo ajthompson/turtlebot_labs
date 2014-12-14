87883cbabe252ef27cfcca88e54072c72c489b5c
Sarah Chamberlain <schamberlain1110@gmail.com>
2014-12-14 17:19:31 -0500

    Oops forgot rospy.spin in play_voice

--- src/turtlebot_navigation_mod/scripts/play_voice.py
+++ src/turtlebot_navigation_mod/scripts/play_voice.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import os
+import rospy
+from std_msgs.msg import Empty
+
 def playVoice(msg):
-os.system("aplay '../sounds/heavy_specialcomplete01.wav'")
+	os.system("aplay '../sounds/heavy_specialcomplete01.wav'")
 
 
 # This is the program's main function
 if __name__ == '__main__':
 	# Change this node name to include your username
 	rospy.init_node('turtlebot_voice_node')
-
+	rospy.spin()
 
 
 	# Subscribers
